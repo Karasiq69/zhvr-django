c58dd3053aff35c1c97c800d12bf9bfc04b992b5
Karasiq69 <128066037+Karasiq69@users.noreply.github.com>
2024-03-06 14:26:29 +0300

    Fixing wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -6,11 +6,27 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
+#
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+#
+# application = get_wsgi_application()
 
 import os
+import sys
+import platform
+
+# путь к проекту
+sys.path.insert(0, '/home/p/pmercedes/varzhar-django/public_html')
+# путь к фреймворку
+sys.path.insert(0, '/home/p/pmercedes/varzhar-django/public_html/backend')
+# путь к виртуальному окружению
+python_version = ".".join(platform.python_version_tuple()[:2])
+sys.path.insert(0, '/home/p/pmercedes/varzhar-django//venv/lib/python{0}/site-packages'.format(python_version))
+os.environ["DJANGO_SETTINGS_MODULE"] = "backend.settings"
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
